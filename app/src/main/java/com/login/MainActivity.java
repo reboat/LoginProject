b7acbdce7d8045ef9917ec1dd6fda2dc714a4b7a
a_liYa <liyaf@8531.cn>
2017-10-10 10:22:57 +0800

    发布1.0.6-SNAPSHOT

--- app/src/main/java/com/login/MainActivity.java
+++ app/src/main/java/com/login/MainActivity.java
@@ -1,7 +1,6 @@
 package com.login;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -27,10 +26,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.tv_text:
-                Nav.with(this).to(Uri.parse("http://www.8531.cn/login/LoginActivity")
-                        .buildUpon()
-                        .build(), 0);
-
+                Nav.with(this).toPath("/login/LoginActivity");
                 break;
             case R.id.test_login:
                 startActivity(new Intent(this, TestLoginActivity.class));
